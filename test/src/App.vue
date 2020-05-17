6f594d5ce5aa7c58b3c8804d7d8ab7218814e8e2
Maharsh Gheewala <maharshgheewala@Maharshs-MacBook-Pro.local>
2020-05-17 17:39:05 -0500

    Final

--- test/src/App.vue
+++ test/src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search..">
+        <input type="text" class="search-bar" placeholder="Search.."
+         v-model="query"
+         @keypress="fetchWeather"
+        />
+        {{query}}
       </div>
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Chicago,IL</div>
-          <div class="date">Sunday 17 May 2020</div>
+          <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
         <div class="weather-box">
-          <div class="temp">12 C</div>
-          <div class="weather">Rainy</div>
+          <div class="temp">{{Math.round(weather.main.temp)}}</div>
+          <div class="weather">{{weather.weather[0].main}}</div>
         </div>
       </div>
     </main>
@@ -24,7 +28,35 @@ export default {
   name: 'App',
   data (){
     return{
-      api_key :"dbdec0d54ecb2d9efd912d38ade50a6e"
+      api_key :"dbdec0d54ecb2d9efd912d38ade50a6e",
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query:'',
+      weather:{}
+    }
+  },
+  methods :{
+    fetchWeather(e){
+      if(e.key == "Enter"){
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res =>{
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults(results){
+      this.weather = results;
+    },
+    dateBuilder(){
+      let d = new Date();
+      let months = ["January", "Febuary", "March", "April", "May", "June","July", "August", "September", "October", "November", "December",];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`
     }
   }
 }
