44e045e00f78bed6388327b398d9fee602948756
Maharsh Gheewala <maharshgheewala@Maharshs-MacBook-Pro.local>
2020-05-17 17:06:51 -0500

    Added Styles

--- test/src/App.vue
+++ test/src/App.vue
@@ -4,6 +4,16 @@
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search..">
       </div>
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Chicago,IL</div>
+          <div class="date">Sunday 17 May 2020</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">12 C</div>
+          <div class="weather">Rainy</div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -27,10 +37,7 @@ export default {
   box-sizing: border-box;
 }
 body{
-  font-family: 'Times New Roman', sans-serif;
-  
-  background-size: cover;
-  transition: 0.4s;
+  font-family: 'montserrat', sans-serif;
 }
 
 #app{
@@ -72,5 +79,50 @@ main{
     box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
     background-color: rgba(255, 255, 255, 0.75);
     border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location{
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date{
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box{
+  text-align: center;
+}
+
+.weather-box .temp{
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 400;
+  font-style: italic;
+
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 6px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather{
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 400;
+  font-style: italic;
 }
 </style>
